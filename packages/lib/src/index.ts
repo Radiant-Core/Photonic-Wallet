11edf1e4d01c2402eb7494243c9a866861a55b6c
TheArtofSatoshi <theartofsatoshi@gmail.com>
2026-01-31 12:42:39 -0600

    Resolve merge conflict in lib package exports
    - Remove merge conflict markers from index.ts
    - Add comprehensive exports for v1/v2 token support
    - Add Glyph v2 feature exports (metadata, burn, royalty, soulbound, container, authority, wave, timelock, crypto, encryption)
    - Add utility exports (difficulty, util, ipfs)

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -1,3 +1,32 @@
 export { coinSelect } from "./coinSelect";
 export { default as Outpoint } from "./Outpoint";
 export { photonsToRXD } from "./format";
+<<<<<<< /Users/main/Downloads/Photonic-Wallet-master/packages/lib/src/index.ts
+=======
+
+// v1 & v2 Token support
+export * from "./token";
+export * from "./protocols";
+export * from "./script";
+export * from "./mint";
+export * from "./tx";
+export * from "./types";
+export * from "./wallet";
+
+// Glyph v2 Features
+export * from "./v2metadata";
+export * from "./burn";
+export * from "./royalty";
+export * from "./soulbound";
+export * from "./container";
+export * from "./authority";
+export * from "./wave";
+export * from "./timelock";
+export * from "./crypto";
+export * from "./encryption";
+
+// Utilities
+export * from "./difficulty";
+export * from "./util";
+export * from "./ipfs";
+>>>>>>> /Users/main/.windsurf/worktrees/Photonic-Wallet-master/Photonic-Wallet-master-234ae55f/packages/lib/src/index.ts
